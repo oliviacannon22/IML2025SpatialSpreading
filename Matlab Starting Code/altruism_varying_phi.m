a64ea54c4888144c49353bba1737d22a1c94ab0d
oliviacannon22 <canno238@umn.edu>
2025-01-29 13:19:15 -0600

    added more comments, hopefully clarify the main step a bit

--- Matlab Starting Code/altruism_varying_phi.m
+++ Matlab Starting Code/altruism_varying_phi.m
@@ -1,5 +1,7 @@
 % Evolve a 2D square domain (want to make rectangular)
-% Boundary conditions set by the Laplacian operator
+% Boundary conditions set by the Laplacian operator (2nd derivative) in
+% both space and altruism parameter 
+%any time you see Y here, it is altruism parameter, and X is space
 
 close all; clear;
 
@@ -31,8 +33,9 @@ dt = 0.01;
 numPar.tf = 1000;  % Final time
 t = 0:dt:numPar.tf;
 iter = length(t);
+n_plot=10; %how often we plot, or every ___ time steps
 
-%disp(['Iter: ' num2str(iter)]);
+disp(['Iter: ' num2str(iter)]);
 
 % Numerical parameters (length of domain and number of grid points)
 %currently, nx and ny have to be the same, but want to change that
@@ -41,7 +44,7 @@ numPar.Ly = 1;
 numPar.Lx = 10;
 numPar.nx = 150;
 numPar.ny = 150;
-numPar.dx = numPar.Lx/(numPar.nx-1); % dx and dy represent the length of cells
+numPar.dx = numPar.Lx/(numPar.nx-1);
 numPar.dy = numPar.Ly/(numPar.ny-1);
 
 numPar.xgrid = 'F_Periodic'; % FD = finite differences, F_Periodic = Fourier, Periodic BC
@@ -55,31 +58,26 @@ y = 0:numPar.dy:numPar.Ly;
 
 
 % Define initial condition 
-% U is a vector containing the density at each value of x and altriusm.
-
-
 %U = 100*ones(numPar.nx,numPar.ny);
 %U(numPar.nx/2:end,numPar.ny/2:end) = 200;
 %U(numPar.nx/2:end,1:numPar.ny/2) = 50;
 %U = U(:);
-U = 1*ones(numPar.nx,numPar.ny); %1st coordinate indexes x position
+U = 1*ones(numPar.nx,numPar.ny);
 U(end-numPar.nx/10:end,numPar.ny/2:end) = 75;
 U(numPar.nx/2:end,1:numPar.ny/2) = 0;
 U(numPar.nx/2-numPar.nx/10:numPar.nx/2,1:numPar.ny/2) = 50;
 U(1:numPar.nx/2,numPar.ny/2:end) = 0;
 U = U + normrnd(0,.2,numPar.nx,numPar.ny);
-U = U(:); %converts matrix to vector by stacking columns on top of each other
+U = U(:);
 
 %"Compute Linear Operator" outputs the matrix for the finite difference
 %approximation of (here) the Laplacian (2nd derivative) 
-%Letting Uxx represent the second x derivative of U, compute linear
-%operator gives the matrix M such that MU = Uxx.
 %boundary conditions given by what we put in for numPar.xgrid (and ygrid)
 %ComputeLinearOperator would output four outputs but we only want the
 %3rd,so that's why the squiggles are there
 
 % Compute Linear Operator for evolution
-[~,~,L2x,~] = ComputeLinearOperator_rectangular(numPar);
+[~,~,L2x,~] = ComputeLinearOperator_rectangular(numPar); 
 L2x = par.kD.*L2x; %multiply componentwise by diffusion coefficient 
 
 %the following part is sort of hacky and is to get a Laplacian
@@ -95,13 +93,14 @@ numPar.Lx=10;
 D2Ux = speye(numPar.nx*numPar.ny) - dt/2*L2x;  %  block matrix - this implementation assumes the same number of x and y gridpoints. 
 D2Uy = speye(numPar.nx*numPar.ny) - dt/2*L2y;  %  block matrix - this implementation assumes the same number of x and y gridpoints.
 % Prepare for first step
-tmp_y1 = groupX(L2y*groupY(U,numPar),numPar);              % Need to group by Y to take Y derivative, then regroup by X . 
+tmp_y1 = groupX(L2y*groupY(U,numPar),numPar);              % this is d^2(U)/dy^2 (explicit term) (Need to group by Y to take Y derivative, then regroup by X) . 
 
 if video_on
     open(v);
 end
 
-   %
+   %plot initial condition 
+   %this step makes a temporary U that is a proper square (vs vector) and plots it, 
    tmpU = reshape(U,numPar.nx,numPar.ny);
                 figure(1); pcolor(x,y,tmpU); shading interp; 
                 colorbar;
@@ -109,33 +108,38 @@ end
 
 
 for k = 1:iter
-        %this is the main step where 
+        %this is the main step where all the work gets done
+        %we use an 'implicit method' to solve equations with 2nd
+        %derivatives. Since we have two of these (d^2/dx^2 and d^2/dy^2), we split dt in two and do
+        %two 'half steps': one for x, one for y
 
         % Grouped by x at beginning of each loop
-        % Implicit in X, explicit in Y   - grouped by x 
+        % First "half step": Implicit in X, explicit in Y   - grouped by x 
        % Evaluate nonlinear term!
        fU = altruismnonlin_varyingphi(U,par,numPar); 
         
        U = D2Ux \ ( U+ dt/2 * tmp_y1 + dt/2 * fU);   % Grouped by X 
+       %equation this is solving:
+       %U_new = U_old + dt/2*(d^2/dx^2(U_new) + d^2/dy^2(U_old) + f(U_old)
 
-        % Implicit in Y, explicit in X - grouped by y
-        tmp_x1 = groupY(L2x*U,numPar);      % currently grouped by x
+        % Second "half step": Implicit in Y, explicit in X - grouped by y
+        tmp_x1 = groupY(L2x*U,numPar);      % this is d^2(U)/dx^2 (explicit term) - (calculate while grouped by X, then regroup in Y)
         
-        
-        
-       % Evaluate nonlinear terms at the most recent values - grouped by Y
-        fU = altruismnonlin_varyingphi(U,par,numPar); % Assumes grouped by X
+       % Evaluate nonlinear terms at the most recent values 
+        fU = altruismnonlin_varyingphi(U,par,numPar); % put into function before switching grouping, bc function takes U grouped by X
         
         U = groupY(U,numPar);   % Switch to Y grouping 
 
         U = D2Uy \ (U + dt/2 * tmp_x1 + dt/2 * groupY(fU,numPar) );  % Grouped by Y
+        %equation this is solving:
+        %U_new = U_old + dt/2*(d^2/dx^2(U_old) + d^2/dy^2(U_new) + f(U_old)
        
         % Get ready for next step
-        tmp_y1 = groupX(L2y*U,numPar);              % multiply before they are flipped 
-        
+        tmp_y1 = groupX(L2y*U,numPar);              % this is d^2(U)/dy^2 again
         U = groupX(U,numPar);   % Switch to X grouping 
        
-         if mod(k,10) == 1
+        %plot current solution 
+         if mod(k,n_plot) == 1
             
                 tmpU = reshape(U,numPar.nx,numPar.ny);
                 figure(1); pcolor(x,y,tmpU); shading interp;
