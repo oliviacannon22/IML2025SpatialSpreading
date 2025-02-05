afd757dc93335513ba7e846978fb1e60831755c4
oliviacannon22 <canno238@umn.edu>
2025-02-05 12:43:15 -0600

    fixed error where y part not properly accounted for

--- Matlab Starting Code/twod_conv_Grc_vary_altruism.m
+++ Matlab Starting Code/twod_conv_Grc_vary_altruism.m
@@ -1,5 +1,5 @@
-function w = twod_conv_Grc_vary_altruism(v1,Nx,Gx,dx)
-%convolve a function v1 with a function G which can be written as a product Gx*Gy
+function w = twod_conv_Grc_vary_altruism(v1,Nx,Gx,dx,dy)
+
 %v1 is a vector representing a function of two variables, which is stacked
 %at present by columns (grouped by X)
 %Nx is number of x grid points
@@ -13,11 +13,11 @@ w=zeros(length(v1),1);
 %define the 1D convolution for x (only different if not a square)
 convx =@(v1,v2) ifft(dx*fft(circshift(v2,Nx/2)).*fft(v1));
 
-%for each fixed x, convolve in the y direction
-% for i = 1:Nx
-%     start = Ny*(i-1) + 1;
-%     v1(start:start+Ny-1)=convy(v1(start:start+Ny-1),Gy);
-% end 
+%for each fixed x, add up in the y direction
+for i = 1:Nx
+     start = Ny*(i-1) + 1;
+     v1(start:start+Ny-1)=sum(v1(start:start+Ny-1))*dy*ones(Ny,1);
+ end 
 %now for each y, convolve with the resulting function in the x
 %direction
 for j = 1:Ny
