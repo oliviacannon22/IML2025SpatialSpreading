62e1e50317d8bb66a9c49f0d0a7c5dd94fdfeeba
oliviacannon22 <canno238@umn.edu>
2025-02-13 09:15:28 -0600

    fixed dimensions for one of the convolutions

--- Matlab Starting Code/altruism_varying_phi.m
+++ Matlab Starting Code/altruism_varying_phi.m
@@ -18,7 +18,7 @@ par.c=1; %cost of altruism to individual
 par.g0=5; %general birth param (probably could be scaled out)
 par.sd_rc=2; %width of competition kernel (should be wider than altruism kernel)
 par.kD=1e-2; %diffusion coeff. of motility
-par.kDy=.001*par.kD; %right now, modeling mutation of altruism by diffusion, much slower than motility diffusion
+par.kDy=.01*par.kD; %right now, modeling mutation of altruism by diffusion, much slower than motility diffusion
 par.K=20; %carrying capacity, roughly speaking
 par.b0=0.5; %param for saturating nonlin
 par.b_max=2; %param for saturating nonlin
@@ -42,7 +42,7 @@ disp(['Iter: ' num2str(iter)]);
 %eventually
 numPar.Ly = 1;
 numPar.Lx = 15;
-numPar.nx = 150;
+numPar.nx = 200;
 numPar.ny = 150;
 numPar.dx = numPar.Lx/(numPar.nx-1);
 numPar.dy = numPar.Ly/(numPar.ny-1);
@@ -65,9 +65,11 @@ y = 0:numPar.dy:numPar.Ly;
  U = 1*ones(numPar.nx,numPar.ny);
  U(end-numPar.nx/10:end,round(5*numPar.ny/8):round(7*numPar.ny/8)) = 5;
  %U(numPar.nx/2:end,1:numPar.ny/2) = 0;
- U(numPar.nx/2-numPar.nx/10:numPar.nx/2,round(numPar.ny/8):round(3*numPar.ny/8)) = 1;
+ %U(numPar.nx/2-numPar.nx/10:numPar.nx/2,round(numPar.ny/8):round(3*numPar.ny/8)) = 5;
 % U(1:numPar.nx/2,numPar.ny/2:end) = 0;
 U = U + normrnd(0,.2,numPar.nx,numPar.ny);
+
+%now make it a vector 
 U = U(:);
 
 
@@ -77,20 +79,7 @@ U = U(:);
 L2x = par.kD.*L2x; %multiply componentwise by diffusion coefficient
 L2y = par.kDy.*L2y;
 
-% % Compute Linear Operator for evolution
-% [~,~,L2x,~] = ComputeLinearOperator_rectangular(numPar); 
-% L2x = par.kD.*L2x; %multiply componentwise by diffusion coefficient
-% 
-% 
-% 
-% %the following part is sort of hacky and is to get a Laplacian
-% %with different boundary conditions and coefficient for when we diffuse in
-% %altruism. but it's ok if it makes no sense
-% numPar.xgrid='FD';numPar.Lx=numPar.Ly;%change the xgrid bc the 2nd deriv always groups by x, but need one Neumann for y
-% %FD = finite differences Neumann 
-% [~,~,L2y,~] = ComputeLinearOperator_rectangular(numPar); 
-% L2y = par.kDy.*L2y;
-% numPar.Lx=10;
+
 
 % Matrix for implicit
 D2Ux = speye(numPar.nx*numPar.ny) - dt/2*L2x;  %  block matrix - this implementation assumes the same number of x and y gridpoints. 
@@ -104,7 +93,7 @@ end
 
    %plot initial condition 
    %this step makes a temporary U that is a proper square (vs vector) and plots it, 
-   tmpU = reshape(U,numPar.nx,numPar.ny);
+   tmpU = reshape(U,numPar.nx,numPar.ny)';
                 figure(1); pcolor(x,y,tmpU); shading interp; 
                 colorbar;
                 drawnow;
@@ -144,7 +133,8 @@ for k = 1:iter
         %plot current solution 
          if mod(k,n_plot) == 1
             
-                tmpU = reshape(U,numPar.nx,numPar.ny);
+                %tmpU = reshape(U,numPar.nx,numPar.ny);
+                tmpU = reshape(U,numPar.nx,numPar.ny)';
                 figure(1); pcolor(x,y,tmpU); shading interp;
                 title(['time=' num2str(k*dt), ' total pop = ' num2str(numPar.dx^2*sum(U)) ]);
                 colorbar;
